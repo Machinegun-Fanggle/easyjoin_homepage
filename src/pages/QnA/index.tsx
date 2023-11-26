4f0eed5a565454bb085e500af7ef158642e94b69
김규현 <rbxlr5848@gamil.com>
2023-11-26 21:25:02 +0900

    ...

--- src/pages/QnA/index.tsx
+++ src/pages/QnA/index.tsx
@@ -2,11 +2,22 @@
 import React, { useState, useEffect, useRef } from "react"
 import styled from "styled-components"
 
+export interface IQnA {
+    title: string
+    content: string
+    category: string
+    createAt: string
+    writer: string
+}
+
 // 메인 컴포넌트 함수
 const QnA = () => {
     const ref = useRef(null)
 
     useEffect(() => {
+        setQnAs(exampleQnAs)
+        setSelectedCategory("전체")
+
         const observer = new IntersectionObserver(
             ([entry]) => {
                 if (entry.isIntersecting) {
@@ -25,13 +36,67 @@ const QnA = () => {
         }
     }, [])
 
+    const handleCategory = (category: string) => {
+        if (category === "전체") {
+            // 선택된 카테고리가 없으면 전체 리스트를 표시
+            setQnAs(exampleQnAs)
+        } else {
+            // 선택된 카테고리와 일치하는 항목만 필터링
+            const filteredQnAs = exampleQnAs.filter((qna) => qna.category === category)
+            setQnAs(filteredQnAs)
+        }
+    }
+
     const [inputValue, setInputValue] = useState<string>("")
     const [selectedCategory, setSelectedCategory] = useState<string>("")
     const keywords = ["키워드 1", "키워드 2", "키워드 3", "키워드 4"]
     const category = ["전체", "서비스 이용 관련", "법적효력", "보안", "기타"]
+    const [expandedQnA_Ids, setExpandedQnA_Ids] = useState<number[]>([])
+    const [qnAs, setQnAs] = useState<IQnA[]>([])
+
+    const exampleQnAs = [
+        {
+            title: "서비스 가입 절차가 궁금합니다",
+            content: "새로운 서비스에 가입하고 싶은데, 어떻게 해야 하나요?",
+            category: "서비스 이용 관련",
+            createAt: "2023-11-27",
+            writer: "NewUser",
+        },
+        {
+            title: "개인정보 보호 정책에 대한 질문",
+            content: "개인정보 보호 정책이 어떻게 적용되는지 자세히 알고 싶습니다.",
+            category: "법적효력",
+            createAt: "2023-11-26",
+            writer: "PrivacySeeker",
+        },
+        {
+            title: "계정 보안 강화 방법",
+            content: "제 계정의 보안을 강화하고 싶습니다. 어떤 조치를 취할 수 있나요?",
+            category: "보안",
+            createAt: "2023-11-25",
+            writer: "SecureUser",
+        },
+        {
+            title: "모바일 앱 오류 보고",
+            content: "모바일 앱에서 오류를 발견했어요. 어디에 보고해야 하나요?",
+            category: "기타",
+            createAt: "2023-11-24",
+            writer: "AppTester",
+        },
+    ]
 
     const handleKeywordClick = (keyword: string) => {
         setInputValue(keyword)
+    }
+
+    const toggleQnA = (id: number) => {
+        setExpandedQnA_Ids((prevIds) => {
+            if (prevIds.includes(id)) {
+                return prevIds.filter((prevId) => prevId !== id)
+            } else {
+                return [...prevIds, id]
+            }
+        })
     }
 
     return (
@@ -87,15 +152,90 @@ const QnA = () => {
                                     width: 100,
                                     marginLeft: "10px",
                                     marginRight: "10px",
+                                    marginBottom: "30px",
                                 }}
                                 isSelected={selectedCategory === category}
-                                onClick={() => setSelectedCategory(category)}
+                                onClick={() => {
+                                    setSelectedCategory(category)
+                                    handleCategory(category)
+                                }}
                             >
                                 {category}
                             </SButton>
                         ))}
                     </div>
-                    <SPageImg src={require("../../assets/images/Frame627.png")} height={1300} />
+
+                    <>
+                        {qnAs.map((QnA, idx) => (
+                            <React.Fragment key={idx}>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        height: "70px",
+                                        width: "100%",
+                                        borderTop: "1px solid #A2A9B0",
+                                        borderBottom: "1px solid #A2A9B0",
+                                        padding: "20px",
+                                        alignItems: "flex-start",
+                                        flexDirection: "column",
+                                        justifyContent: "center",
+                                    }}
+                                >
+                                    <div
+                                        onClick={() => toggleQnA(idx)}
+                                        style={{
+                                            display: "flex",
+                                            width: "100%",
+                                        }}
+                                    >
+                                        <Wrapper style={{ width: "100%", padding: "0" }}>
+                                            <SubTitle>
+                                                <p style={{ fontWeight: 600, marginRight: "10px" }}>
+                                                    [{QnA.category}]
+                                                </p>
+                                                {QnA.title}
+                                            </SubTitle>
+                                        </Wrapper>
+                                        <div
+                                            style={{
+                                                width: "5%",
+                                                color: "#A2A9B0",
+                                                display: "flex",
+                                                justifyContent: "center",
+                                                alignItems: "center",
+                                            }}
+                                        >
+                                            {expandedQnA_Ids.includes(idx) ? "▲" : "▼"}
+                                        </div>
+                                    </div>
+                                </div>
+                                {expandedQnA_Ids.includes(idx) && (
+                                    <div
+                                        style={{
+                                            display: "flex",
+                                            width: "100%",
+                                            background: "#f2f4f8",
+                                            borderBottom: "1px solid #A2A9B0",
+                                            padding: "20px",
+                                            alignItems: "flex-start",
+                                            flexDirection: "column",
+                                            justifyContent: "center",
+                                        }}
+                                    >
+                                        <SubTitle style={{ fontSize: "13px" }}>
+                                            <p
+                                                dangerouslySetInnerHTML={{
+                                                    __html: QnA.content.replace(/\n/g, "<br />"),
+                                                }}
+                                            />
+                                        </SubTitle>
+                                    </div>
+                                )}
+                            </React.Fragment>
+                        ))}
+                    </>
+
+                    {/* <SPageImg src={require("../../assets/images/Frame627.png")} height={1300} /> */}
                 </div>
             </SPage1>
         </SPageWrapper>
@@ -203,4 +343,31 @@ const SPageImg = styled.img`
     margin: 80px 0 0 0;
     width: 1200px;
     border-radius: 20px;
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 100vw;
+    height: 100%;
+
+    justify-content: center;
+
+    padding: 20px;
+
+    font-size: 14px;
+    color: #5157e0;
+    font-family: Poppins;
+`
+const SubTitle = styled.p`
+    display: flex;
+
+    font-size: clamp(0.1rem, 4vw, 1.3rem);
+    color: #21242b;
+
+    @media screen and (max-width: 768px) {
+        justify-content: left;
+        align-items: flex-start;
+    }
 `
