dca4e2f65b13181c1eb3fc77c06dc3c148134621
김규현 <rbxlr5848@gamil.com>
2023-11-27 20:39:23 +0900

    ...

--- src/pages/TermsOfUse/index.tsx
+++ src/pages/TermsOfUse/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef, useState } from "react"
 import styled from "styled-components"
 import SearchIcon from "../../assets/search-md.svg"
+import { useNavigate } from "react-router-dom"
 
 // 이용약관
 const TermsOfUse = () => {
     const ref = useRef(null)
     const dropdownRef = useRef<HTMLDivElement | null>(null)
+    const movePage = useNavigate()
 
     // 검색어와 검색 결과 상태 관리
     const [searchText, setSearchText] = useState("")
@@ -63,6 +65,16 @@ const TermsOfUse = () => {
         }
     }, [])
 
+    const [activeTab, setActiveTab] = useState("tab1")
+
+    const handleTabClick = (tab: string) => {
+        if (tab === "tab2") {
+            movePage("/privacy-policy")
+        }
+
+        setActiveTab(tab)
+    }
+
     return (
         <SPageWrapper ref={ref}>
             <SPage1>
@@ -74,9 +86,23 @@ const TermsOfUse = () => {
                         width: "1200px",
                     }}
                 >
-                    <SMainText style={{ color: "#000", fontSize: "42px", textAlign: "center" }}>
+                    {/* <SMainText style={{ color: "#000", fontSize: "42px", textAlign: "center" }}>
                         이용약관
-                    </SMainText>
+                    </SMainText> */}
+                    <TabsContainer>
+                        <Tab
+                            className={activeTab === "tab1" ? "active" : ""}
+                            onClick={() => handleTabClick("tab1")}
+                        >
+                            이용약관
+                        </Tab>
+                        <Tab
+                            className={activeTab === "tab2" ? "active" : ""}
+                            onClick={() => handleTabClick("tab2")}
+                        >
+                            개인정보처리방침
+                        </Tab>
+                    </TabsContainer>
                 </div>
             </SPage1>
         </SPageWrapper>
@@ -120,4 +146,49 @@ const SPage1 = styled.div`
     padding: 200px 364px;
     justify-content: center;
     align-items: center;
+`
+const TabsContainer = styled.div`
+    display: flex;
+    width: 100%;
+    height: 48px;
+    border-bottom: 1px solid #dde1e6;
+`
+
+const Tab = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 10px 15px;
+    cursor: pointer;
+    border: 1px solid #dde1e6;
+    border-bottom: none;
+    background-color: white;
+
+    &:hover,
+    &.active {
+        background-color: #f2f2f2;
+        font-weight: bold;
+    }
+`
+
+const H1 = styled.div`
+    margin: 100px 0 0 0;
+    line-height: 24px;
+    color: #333333;
+    font-size: 16;
+    font-family: Pretendard;
+    font-weight: 700;
+    word-wrap: break-word;
+`
+
+const H2 = styled.p`
+    color: "#555555";
+    font-size: 15px;
+    font-family: "Pretendard", sans-serif; // Font family
+    font-weight: "400";
+    line-height: 24px;
+    white-space: pre-wrap; // 줄바꿈은 유지하되, 필요한 경우 자동으로 줄바꿈을 추가합니다.
+    word-wrap: break-word; // 긴 단어가 있을 경우 자동으로 단어를 줄바꿈합니다.
+    overflow-wrap: break-word; // 내용이 너무 길 경우 컨테이너 밖으로 넘어가지 않도록 줄바꿈합니다.
 `
